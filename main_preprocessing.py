03eff2de766e56da855ccc102e76808f1f4ac7c5
Elpida99 <61232574+Elpida99@users.noreply.github.com>
2021-04-19 17:01:43 +0300

    Update main_preprocessing.py

--- main_preprocessing.py
+++ main_preprocessing.py
@@ -1,7 +1,7 @@
-from my_methods import load_data, get_all_labels, normalize_data, fix_length, plot_signals, bandpass_with_lfilter
-from data_loader import get_classes
 import numpy as np
 import h5py
+from my_methods import (bandpass_filter, fix_length, load_data, 
+                        normalize_data, resample_data, transpose_data)
 
 # ----------------------------------------------------------------------------------------
 # Preprocessing (500 Hz freq, 7500 points for each recording(each lead) and T = 15 sec)
@@ -12,23 +12,72 @@ import h5py
 path1 = r'C:\Users\elpid\PycharmProjects\Thesis\Data_500_Hz_2\Dataset_1'
 path2 = r'C:\Users\elpid\PycharmProjects\Thesis\Data_500_Hz_2\Dataset_2'
 
-def preprocessing(path):
+
+def main_preprocessing(path, length):
+  """
+  Normalizes the data to [-1, 1] and if pretrained is True, they are resampled
+  Applies zero padding/ cuts the recordings to a fixed length
+  Applies banpass filter
+  :param path: path to dataset
+  :param length: desired length of recordings
+  :param pretrained: if the data are meant for the pretrained model then set to True
+  :return: the final processed data
+  """
   data, header_data = load_data(path)
 
   normalized_data = normalize_data(data)
 
-  unfiltered_recordings = fix_length(normalized_data, 7500)
+  unfiltered_recordings = fix_length(normalized_data, length)
 
-  filtered_recordings = bandpass_with_lfilter(unfiltered_recordings)
+  filtered_recordings = bandpass_filter(unfiltered_recordings)
 
   return filtered_recordings
 
-dataset1 = preprocessing(path1)
-dataset2 = preprocessing(path2)
 
-combined_datasets = np.append(dataset1, dataset2, axis=0)
-print(combined_datasets.shape)
+def main_data(path1, path2):
+  dataset1 = main_preprocessing(path1, 7500)
+  dataset2 = main_preprocessing(path2, 7500)
 
-with h5py.File(r'C:\Users\elpid\PycharmProjects\Thesis\data\ecg_tracings.hdf5', 'w') as hdf:
-     hdf['tracings'] = combined_datasets
-hdf.close()
+  combined_datasets = np.append(dataset1, dataset2, axis=0)
+  print(combined_datasets.shape)
+
+  with h5py.File(r'C:\Users\elpid\PycharmProjects\Thesis\data\ecg_tracings.hdf5', 'w') as hdf:
+       hdf['tracings'] = combined_datasets
+  hdf.close()
+
+
+def pretrained_preprocessing(path, length):
+  data, header_data = load_data(path)
+
+  data = resample_data(data, 500, 400)
+
+#  normalized_data = normalize_data(data)
+
+  unfiltered_recordings = fix_length(data, length)
+
+#  filtered_recordings = bandpass_filter(unfiltered_recordings)
+
+  final_data = transpose_data(unfiltered_recordings)
+
+  return final_data
+
+
+def pretrained_data(path1, path2):
+  """
+  :param path1: path to dataset 1
+  :param path2: path to dataset 2
+  :saves the data in order to be feeded in the pretrained model
+  """
+  dataset1 = pretrained_preprocessing(path1, 4096)
+  dataset2 = pretrained_preprocessing(path2, 4096)
+
+  combined_datasets = np.append(dataset1, dataset2, axis=0)
+  print(combined_datasets.shape)
+
+  with h5py.File(r'C:\Users\elpid\PycharmProjects\Thesis\data_400\400_ecg_tracings.hdf5', 'w') as hdf:
+    hdf['tracings'] = combined_datasets
+  hdf.close()
+
+# main_data(path1, path2)
+# pretrained_data(path1, path2)
+
